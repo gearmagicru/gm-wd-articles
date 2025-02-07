7f08badd8e4d62e372d65834d4eefbfa87abf4c4
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-07 15:39:08 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Articles',
+    '{description}' => 'Site articles'
+];
